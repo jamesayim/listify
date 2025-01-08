6b85e2dce1ad3f8176aebbc2f9b9ca2f5001305c
jamesyim <theladjames@gmail.com>
2025-01-08 12:19:35 +0000

    enhance responsive design: add media queries for mobile and tablet views; adjust layout and styles for login and signup pages; improve button functionality for navigation

--- src/client/index.js
+++ src/client/index.js
@@ -5,6 +5,8 @@ class HandlePageEntry {
         this.page = document.body.dataset.page;
         this.loginBtn = document.getElementById("login-btn");
         this.signupBtn = document.getElementById("signup-btn");
+        this.trynowBtn = document.getElementById("try-for-free-btn");
+        this.learnmoreBtn = document.getElementById("learn-more-btn");
     }
     sendToLoginPage() {
         window.location.href = "./login.html";
@@ -12,16 +14,24 @@ class HandlePageEntry {
     sendToSignupPage() {
         window.location.href = "./signup.html";
     }
+    sendToSignupPageFromTrynow() {
+        window.location.href = "./signup.html";
+    }
+    sendToAboutPage() {
+        window.location.href = "./about.html";
+    }
     addEventListeners() {
         this.loginBtn.addEventListener("click", this.sendToLoginPage.bind(this));
         this.signupBtn.addEventListener("click", this.sendToSignupPage.bind(this));
+        this.trynowBtn.addEventListener("click", this.sendToSignupPageFromTrynow.bind(this));
+        this.learnmoreBtn.addEventListener("click", this.sendToAboutPage.bind(this));
     }
 }
 
 const pageEntry = new HandlePageEntry();
 
 if (pageEntry.page === "home") {
-    if (pageEntry.loginBtn || pageEntry.signupBtn) {
+    if (pageEntry.loginBtn || pageEntry.signupBtn || pageEntry.trynowBtn || pageEntry.learnmoreBtn) {
         pageEntry.addEventListeners();
     }
 } else if (pageEntry.page === "login") {
