f4dffc38ec1b27451943fc0f8bc25ed64ba04287
jamesyim <theladjames@gmail.com>
2025-01-11 22:30:54 +0000

    feat: implement responsive hamburger menu for navigation; enhance mobile layout and styles

--- src/client/index.js
+++ src/client/index.js
@@ -7,6 +7,8 @@ class HandlePageEntry {
         this.signupBtn = document.getElementById("signup-btn");
         this.trynowBtn = document.getElementById("try-for-free-btn");
         this.learnmoreBtn = document.getElementById("learn-more-btn");
+        this.hamburgerSvgBtn = document.getElementById("hamburger-svg");
+        this.hamburgerMenuElement = document.getElementById("navlist");
     }
     sendToLoginPage() {
         window.location.href = "./login.html";
@@ -20,15 +22,49 @@ class HandlePageEntry {
     sendToAboutPage() {
         window.location.href = "./about.html";
     }
+    toggleHamburgerMenu() {
+        const menu = this.hamburgerMenuElement;
+        if (menu.style.display === "none" || menu.style.display === "") {
+            menu.style.display = "flex";
+            this.hamburgerSvgBtn.style.marginLeft = "0";
+        } else {
+            menu.style.display = "none";
+            this.hamburgerSvgBtn.style.marginLeft = "280px";
+            if (window.innerWidth <= 375) {
+                this.hamburgerSvgBtn.style.marginLeft = "280px";
+            }
+            else if (window.innerWidth <= 425) {
+                this.hamburgerSvgBtn.style.marginLeft = "200px";
+            } else {
+                this.hamburgerSvgBtn.style.marginLeft = "0";
+            }
+        }
+    }
+    initializeHamburgerSvgBtn() {
+        if (window.innerWidth <= 425 && this.hamburgerSvgBtn) {
+            const maxMargin = window.innerWidth - this.hamburgerSvgBtn.offsetWidth;
+            const marginLeft = Math.min(200, maxMargin);
+            this.hamburgerSvgBtn.style.marginLeft = `${marginLeft}px`;
+            this.hamburgerSvgBtn.style.transition = "margin-left 0.3s ease";
+        } else if (window.innerWidth >= 1024) {
+                this.hamburgerMenuElement.style = "flex";
+            } else {
+                this.hamburgerSvgBtn.style.marginLeft = "0";
+        }
+    }
     addEventListeners() {
         this.loginBtn.addEventListener("click", this.sendToLoginPage.bind(this));
         this.signupBtn.addEventListener("click", this.sendToSignupPage.bind(this));
         this.trynowBtn.addEventListener("click", this.sendToSignupPageFromTrynow.bind(this));
         this.learnmoreBtn.addEventListener("click", this.sendToAboutPage.bind(this));
+        this.hamburgerSvgBtn.addEventListener("click", this.toggleHamburgerMenu.bind(this));
+        window.addEventListener("resize", this.initializeHamburgerSvgBtn.bind(this));
     }
 }
 
 const pageEntry = new HandlePageEntry();
+
+pageEntry.initializeHamburgerSvgBtn();
 
 if (pageEntry.page === "home") {
     if (pageEntry.loginBtn || pageEntry.signupBtn || pageEntry.trynowBtn) {
